60b9269a12c39c72d719958323d3b2f487625a4d
odeliachen <odeliachen@webank.com>
2020-06-13 18:23:19 +0800

    Updates

---
+++ precache-manifest.4cbca3ddd0b15242a92871a5b0761540.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85767e79cc17237e931ac54d7da158a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "350b1bf57699c9303ef7",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1c71c32799a056d33b85",
+    "url": "/static/js/2.6155f862.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6155f862.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "350b1bf57699c9303ef7",
+    "url": "/static/js/main.e77994c5.chunk.js"
+  },
+  {
+    "revision": "b6ebbf9d3a57d0f1ef9d",
+    "url": "/static/js/runtime-main.5eeb4e9f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
